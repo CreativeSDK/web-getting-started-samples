a1a5b1cff7b958ed13a2718f1516dc7d0edb18ae
Ash <ash@ashryanbeats.com>
2016-04-12 13:12:33 -0400

    Added Image Editor configuration and launch button click handler.

---
+++ image-editor-ui-jquery/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	var originalImageSrc = $('#editiable-image').attr('src');
+	var currentImage; // assigned when the Edit button is clicked
+
+	// Image Editor configuration
+	var csdkImageEditor = new Aviary.Feather({
+		apiKey: '<YOUR_KEY_HERE>',
+		onSave: function(imageID, newURL) {
+			currentImage.src = newURL;
+			csdkImageEditor.close();
+			console.log(newURL);
+		},
+		onError: function(errorObj) {
+			console.log(errorObj.code);
+			console.log(errorObj.message);
+			console.log(errorObj.args);
+		}
+	});
+
+	// Launch Image Editor
+	$('#edit-image-button').click(function() {
+
+		// Get the image to be edited
+		// `[0]` gets the image itself, not the jQuery object
+		currentImage = $('#editable-image')[0];
+
+		csdkImageEditor.launch({
+			image: currentImage.id,
+			url: currentImage.src
+		});
+	});
+
+});
