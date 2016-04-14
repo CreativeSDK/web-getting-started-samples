4290a9c54d3e1b20668c773d5be3d2afff81e4d9
Ash <ash@ashryanbeats.com>
2016-04-14 15:52:45 -0400

    Added routes for /api dir.

--- image-editor-angular-express/server/app.js
+++ image-editor-angular-express/server/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var chalk = require('chalk');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var clientPath = path.join(__dirname, '../client');
 var buildPath = path.join(__dirname, '../client/build');    // for gulped files
@@ -42,6 +43,17 @@ app.use(function (req, res, next) {
 });
 
 // Routes
+//// APIs for AJAX
+
+// Look up all route files/folders from directory
+var directories = fs.readdirSync(path.join(__dirname, '/api/'));
+
+// Require each route dynamically 
+directories.forEach(function(dir) {
+  // Prepend /api/ to all api routes
+  app.use('/api/' + dir + '/', require('./api/' + dir));
+});
+
 //// Index/Home
 app.use('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, './index.html'));
