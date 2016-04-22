b4f38c3d4be8bace04e39d1770d5c31584237f17
Ash <ash@ashryanbeats.com>
2016-04-22 17:15:56 -0400

    Some initial work on the API endpoint for the signature.

--- image-editor-ui-angular-express-hires/server/api/auth/auth.controller.js
+++ image-editor-ui-angular-express-hires/server/api/auth/auth.controller.js
@@ -1,11 +1,46 @@
 var http = require('http');
+var uuid    = require('node-uuid');
+var crypto  = require('crypto');
+
 var apiKeys = require('../../apiKeys.js');
+
 
 module.exports = {
 	creativesdk: function(req, res) {
 
-		console.log(apiKeys.creativeSdkKey);
+		// console.log(apiKeys.creativeSdkKey);
 
-		res.send(apiKeys.creativeSdkKey);
+		// res.send(apiKeys.creativeSdkKey);
 	}
+}
+
+
+function getAuth() {
+
+    var authObj = {
+        apiKey: apiKeys.creativeSdkKey,
+        secret: apiKeys.creativeSdkSecret,
+        salt: getSalt(),
+        timestamp: getUnixTimestamp().toString(),
+        encryptionMethod: "sha1"
+    };
+
+    authObj.signature = getSignature(authObj.apiKey, authObj.secret, authObj.salt, authObj.timestamp, authObj.encryptionMethod);
+
+    return authObj;
+}
+
+function getSalt() {
+	return uuid.v4();
+}
+
+function getUnixTimestamp() {
+	return (new Date()).getTime() / 1000;
+}
+
+function getSignature(key, secret, salt, timestamp, encryptionMethod) {
+
+	let sig = key + secret + timestamp + salt;
+
+	return crypto.createHash(encryptionMethod).update(sig).digest('hex');
 }
