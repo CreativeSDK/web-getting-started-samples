58d6920226c59ab6896346505d149cadacce46aa
Ash <ash@ashryanbeats.com>
2017-11-20 18:29:19 -0500

    File update for v1.0.1.

--- image-editor-ui/image-editor-ui-angular-express-hi-res/server/api/auth/index.js
+++
@@ -1,8 +0,0 @@
-var router = require('express').Router();
-
-var controller = require('./auth.controller.js');
-
-module.exports = router;
-
-router.get('/creativesdkAuthObj', controller.creativesdkAuthObj);
-router.get('/creativesdkKey', controller.creativesdkKey);
--- image-editor-ui/image-editor-ui-angular-express/client/pre-build/navbar/navbar.directive.js
+++
@@ -1,6 +0,0 @@
-app.directive("navbar", function(){
-	return {
-		restrict: "E",
-		templateUrl: "/pre-build/navbar/navbar.html"
-	};
-});
